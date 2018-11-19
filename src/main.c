0b2802af35d968a373370063c5dbe4ee91f1aaca
Champagne Lewis <champagne.t.lewis@gmail.com>
2018-11-18 23:24:35 -0500

    Set up Check skeleton for use with CMake
    Copied over the contents of the Check CMake example, commenting
    out the example-specific bits for reference. The current files
    ARE NOT ready for building, and most likely will result in an
    error on an attempted `cmake .`. That said, it should be trivial
    to fit the code from the development branch into this skeleton.

---
+++ src/main.c
@@ -0,0 +1,12 @@
+// commented out part kept from example as a reference
+//#include "money.h"
+
+/* only main should be in this file, to make all other functions in
+   the program testable by Check.  in order to test main(), use a
+   whole program testing framework like Autotest.
+*/
+
+int main(void)
+{
+    return 0;
+}
